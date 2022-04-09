fee0651265040796903912a805fc17333fa64177
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-09 17:03:43 +0530

    Added : Java Program to Check Whether a Number is Even or Odd

---
+++ src/com/bridgelabz/coreprograms/OddEven.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.coreprograms;
+
+import Utility.Util;
+
+/**
+ * Java Program to Check Whether a Number is Even or Odd
+ * 
+ * @author Snehal Patil
+ *
+ */
+public class OddEven {
+	public static void main(String[] args) {
+		Util.checkEvenOdd();
+	}
+}
