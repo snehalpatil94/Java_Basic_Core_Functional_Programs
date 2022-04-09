34f22e0520fb606008dbc102dab401b50cb3b3c2
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-09 16:56:24 +0530

    Added : Program to Flip Coin and print percentage of Heads and Tails

---
+++ src/com/bridgelabz/coreprograms/FlipCoin.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.coreprograms;
+
+import Utility.Util;
+
+/**
+ * Flip Coin and print percentage of Heads and Tails
+ * 
+ * @author : Snehal Patil
+ *
+ */
+public class FlipCoin {
+	public static void main(String[] args) {
+		Util.flipCoin();
+	}
+}
